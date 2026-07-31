3981a52895333df596136d7a3678bf8045db4b4a
brockbank9 <brockbank9@gmail.com>
2026-07-31 17:00:05 -0600

    Add neutral AI Professional presenter configuration for v1.2.1

---
+++ ret02-calculator-v1-2/presenter-config.js
@@ -0,0 +1,15 @@
+window.AI_PRO_CONFIG = Object.freeze({
+  release: '1.2.1',
+  presenter: 'presenter-default',
+  presenters: Object.freeze({
+    'presenter-default': Object.freeze({
+      label: 'Default Presenter',
+      cssClass: 'presenter-default'
+    }),
+    'presenter-alt': Object.freeze({
+      label: 'Alternate Presenter',
+      cssClass: 'presenter-alt',
+      archived: true
+    })
+  })
+});
